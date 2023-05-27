046e72a5197ecc75cce044417c23705b4f058bdf
Sasha Pletneva <pletnyovski@gmail.com>
2023-05-27 22:59:40 +0300

    change functions

--- 07/fetcher.py
+++ 07/fetcher.py
@@ -3,31 +3,59 @@ import asyncio
 import sys
 
 import aiohttp
+from bs4 import BeautifulSoup
 
 
-def get_urls(filename):
+async def fetch_url(session, queue):
+    results = []
+    while not queue.empty():
+        url = await queue.get()
+        try:
+            async with session.get(url) as resp:
+                content = await resp.read()
+                soup = BeautifulSoup(content, features='html.parser')
+                title = soup.find('title').string
+                results.append(title)
+        except Exception as exc:
+            raise Exception(f"Error fetching {url}") from exc
+        finally:
+            queue.task_done()
+    return results
+
+
+async def fetch_several_url(filename, max_req):
+    que = await get_queue_urls(filename)
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        titles = []
+        while not que.empty():
+            tasks.append(fetch_url(session, que))
+
+            if len(tasks) >= max_req:
+                results = await asyncio.gather(*tasks)
+                for result in results:
+                    for res in result:
+                        titles.append(res)
+                tasks = []
+        if tasks:
+            results = await asyncio.gather(*tasks)
+            for result in results:
+                for res in result:
+                    titles.append(res)
+        return titles
+
+
+async def get_queue_urls(filename):
+    queue = asyncio.Queue()
     try:
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             for line in file:
-                yield line.strip()
+                url = line.strip()
+                await queue.put(url)
+            return queue
     except FileNotFoundError as exc:
         raise FileNotFoundError(f'{filename} not found') from exc
-
-
-async def fetch_url(url, sem):
-    async with aiohttp.ClientSession() as session:
-        async with sem:
-            try:
-                async with session.get(url) as resp:
-                    return await resp.read()
-            except Exception as exc:
-                raise Exception(f"Error fetching {url}") from exc
-
-
-async def fetch_several_url(urls, max_req):
-    semaphore = asyncio.Semaphore(max_req)
-    tasks = [asyncio.create_task(fetch_url(url, semaphore)) for url in urls]
-    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
@@ -35,5 +63,4 @@ if __name__ == "__main__":
     parser.add_argument('-c', default=0, type=int)
     parser.add_argument('-f', default=0, type=str)
     input_args = parser.parse_args(sys.argv[1:])
-    URLS = get_urls(input_args.f)
-    asyncio.run(fetch_several_url(URLS, input_args.c))
+    asyncio.run(fetch_several_url(input_args.f, input_args.c))
--- 07/fetcher_test.py
+++ 07/fetcher_test.py
@@ -1,42 +1,72 @@
 import asyncio
-import time
 from unittest import mock, IsolatedAsyncioTestCase
-from unittest.mock import mock_open
-from urllib import request
+from unittest.mock import MagicMock
 
-from fetcher import get_urls, fetch_url, fetch_several_url
+from aiohttp import ClientSession
+from aiohttp.test_utils import make_mocked_coro
+
+from fetcher import fetch_url, fetch_several_url, get_queue_urls
 
 
 class TestFetcher(IsolatedAsyncioTestCase):
-    def test_get_urls(self):
-        with self.assertRaises(FileNotFoundError) as err:
-            urls = get_urls('qwerty.txt')
-            next(urls)
-        self.assertEqual(str(err.exception), 'qwerty.txt not found')
-        file_content = 'https://en.wikipedia.org/wiki/United_States'
-        with mock.patch('builtins.open', mock_open(read_data=file_content)):
-            result = list(get_urls('test.txt'))
-            self.assertEqual(result, [file_content])
+
+    async def test_get_queue_urls(self):
+        mock_queue = mock.AsyncMock()
+        mock_file = mock.mock_open(read_data="https://example.com")
+        with mock.patch("builtins.open", mock_file):
+            await get_queue_urls("urls.txt")
+
+        mock_queue.put.assert_called_once_with("https://example.com")
 
     async def test_fetch_url(self):
-        file_content = 'https://ed_Stas'
-        with mock.patch('builtins.open', mock_open(read_data=file_content)):
-            result = next(get_urls('test.txt'))
-        with self.assertRaises(Exception) as err:
-            await fetch_url(result, asyncio.Semaphore(1))
-        self.assertEqual(str(err.exception), f'Error fetching {file_content}')
-        file_content = 'https://en.wikipedia.org/wiki/United_States'
-        with mock.patch('builtins.open', mock_open(read_data=file_content)):
-            result = next(get_urls('test.txt'))
-        data = await fetch_url(result, asyncio.Semaphore(1))
-        self.assertEqual(data, request.urlopen(file_content).read())
+        mock_response = MagicMock()
+        mock_response.read = make_mocked_coro('<html><title>Page Title</title></html>')
+        mock_session = mock.MagicMock(spec=ClientSession)
+        mock_session.get.return_value.__aenter__.return_value = mock_response
 
-    async def test_several_url(self):
-        urls = get_urls('urls.txt')
-        time1 = time.time()
-        await fetch_several_url(urls, 10)
-        result1 = time.time() - time1
-        time2 = time.time()
-        await fetch_several_url(urls, 100)
-        result2 = time.time() - time2
-        self.assertTrue(result2 < result1)
+        mock_queue = asyncio.Queue()
+        await mock_queue.put('https://example.com')
+
+        with mock.patch('aiohttp.ClientSession', return_value=mock_session):
+            results = await fetch_url(mock_session, mock_queue)
+            self.assertEqual(len(results), 1)
+            self.assertEqual(results[0], 'Page Title')
+
+    @mock.patch('fetcher.get_queue_urls')
+    @mock.patch('fetcher.fetch_url')
+    @mock.patch('builtins.open', new_callable=mock.mock_open)
+    def test_fetch_several_url(self, mock_open, mock_fetch_url, mock_get_queue_urls):
+        queue = asyncio.Queue()
+        queue.put_nowait('http://example.com')
+        queue.put_nowait('http://example.org')
+
+        # Настройка мок-объекта get_queue_urls
+        mock_get_queue_urls.return_value.return_value = queue  # Исправленная строка
+        mock_fetch_url.return_value = ['Title 1', 'Title 2']
+
+        mock_file_contents = 'test_file_contents'
+        mock_open.return_value.__enter__.return_value.read.return_value = mock_file_contents
+
+        result = asyncio.run(fetch_several_url('test_filename', 2))
+
+        self.assertEqual(result, ['Title 1', 'Title 2'])
+        mock_get_queue_urls.assert_called_once_with('test_filename')
+        mock_fetch_url.assert_called()
+        mock_open.assert_called_once_with('test_filename', 'r')
+
+        mock_open.reset_mock()
+        mock_fetch_url.reset_mock()
+        mock_get_queue_urls.reset_mock()
+
+        queue = asyncio.Queue()
+        queue.put_nowait('http://example.com')
+
+        mock_get_queue_urls.return_value.return_value = queue
+        mock_fetch_url.return_value = ['Title 3']
+
+        result = asyncio.run(fetch_several_url('another_filename', 1))
+
+        self.assertEqual(result, ['Title 3'])
+        mock_get_queue_urls.assert_called_once_with('another_filename')
+        mock_fetch_url.assert_called()
+        mock_open.assert_called_once_with('another_filename', 'r')
